06d1805845b8f5578bb89f5c157187dd83c0ff17
vedant_dhamecha <vedantdhamecha9525@gmail.com>
2023-10-29 14:10:31 +0530

    about to intro

--- app/sections/NavBar.tsx
+++ app/sections/NavBar.tsx
@@ -44,7 +44,7 @@ const NavBar = () => {
                         aria-label="Scroll to About Section"
                     >
                         <h4 className="py-2 px-2 text-[12px] sm:px-4 sm:text-[14px] md:py-1 md:px-4">
-              ABOUT
+              INTRO
                         </h4>
                     </Link>
 
